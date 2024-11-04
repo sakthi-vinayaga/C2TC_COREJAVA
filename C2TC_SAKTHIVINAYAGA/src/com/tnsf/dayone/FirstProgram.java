13f6768e55307f0d2ae88bb6489847f1c77a2127
karthik <karthik@LAPTOP-OTO5LGQ2>
2024-11-04 10:02:24 +0530

    GTRTT

---
+++ C2TC_SAKTHIVINAYAGA/src/com/tnsf/dayone/FirstProgram.java
@@ -0,0 +1,11 @@
+package com.tnsf.dayone;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
---
+++ C2TC_SAKTHIVINAYAGA/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author karthik
+ *
+ */
+module C2TC_SAKTHIVINAYAGA {
+}
