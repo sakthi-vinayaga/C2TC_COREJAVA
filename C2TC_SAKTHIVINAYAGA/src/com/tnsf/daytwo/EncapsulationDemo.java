f2aa440237766a2006dcdbbe2fc1e4d97f81d01f
karthik <karthik@Baadu>
2024-11-04 15:37:47 +0530

    hi

---
+++ C2TC_SAKTHIVINAYAGA/src/com/tnsf/daytwo/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tnsf.daytwo;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//object creation  
+		OopsConceptDemo obj = new 
+		OopsConceptDemo(); 
+		obj.setSerialNum(134); 
+		obj.setName("sakthi"); 
+		obj.setAge(21); 
+		System.out.println(obj);
+	}
+
+}
